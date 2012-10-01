035853a57a39f28bcfeb7f67c7e18aeb471faf2e
uams <uams@euclid>
2012-09-30 23:18:12 -0700

    not needed. will do sometime else

--- package_managers/filepicker.rb
+++
@@ -1,9 +0,0 @@
-# This is for submission to Homebrew
-
-requre 'formula'
-
-class filepicker < GithubGistFormula
-    url 'https://raw.github.com/uams/filepicker-cli/master/filepicker'
-    homepage 'https://github.com/uams/filepicker-cli'
-    sha1 '5f8a5f2ad12c242d8c0cb0d13e4931550029b69f'
-end
