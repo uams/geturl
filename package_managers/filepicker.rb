b992d1b59e4df3536f3b66d7f9d7d39df199a41f
uams <uams@euclid>
2012-09-30 22:58:41 -0700

    new sha1 for previous commit

--- package_managers/filepicker.rb
+++ package_managers/filepicker.rb
@@ -5,5 +5,5 @@ requre 'formula'
 class filepicker < GithubGistFormula
     url 'https://raw.github.com/uams/filepicker-cli/master/filepicker'
     homepage 'https://github.com/uams/filepicker-cli'
-    sha1 'c128a721b0eff2d570468a527d427b96d55a981b'
+    sha1 '5f8a5f2ad12c242d8c0cb0d13e4931550029b69f'
 end
